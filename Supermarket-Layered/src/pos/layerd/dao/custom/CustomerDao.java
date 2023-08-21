74c6d63bb9ecb659e0eedef0605c219a8579204f
hiranya22 <141381165+hiranya22@users.noreply.github.com>
2023-08-21 13:21:15 +0530

    Add files via upload

---
+++ Supermarket-Layered/src/pos/layerd/dao/custom/CustomerDao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pos.layerd.dao.custom;
+
+import pos.layerd.dao.CrudDao;
+import pos.layerd.entity.CustomerEntity;
+
+/**
+ *
+ * @author Hiranya
+ */
+public interface CustomerDao extends CrudDao<CustomerEntity, String>{
+    
+}
